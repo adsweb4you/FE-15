3bc6fdc97b099f02e1e0cec06d5f66bfd70d7c99
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-06-07 19:46:22 +0400

    js ტიპები

---
+++ js/mine.js
@@ -0,0 +1,54 @@
+//  let nav =  document.querySelector("nav");
+//  let circe =  document.querySelector(".circle");
+
+
+//  window.addEventListener("scroll", function(event){
+//    if(this.scrollY > 50){
+//     nav.style.backgroundColor = "red";
+//    }else{
+//     nav.style.backgroundColor = "black";
+//    }
+//  })
+
+
+//  window.addEventListener("mousemove", function(event){
+//    console.log(event.screenX,  event.screenY)
+
+//    circe.style.left = event.screenX + "px";
+//    circe.style.top = event.screenY + "px";
+//   })
+
+
+// let btn  = document.querySelector(".subm");
+
+// btn.addEventListener("click", function(){
+
+//     let price = 5;
+
+//     let weight = document.querySelector("#weight").value;
+//     let count = document.querySelector("#count").value;
+
+//     let sum  = ( price * Number(weight) ) * Number(count);
+//     output.innerText = sum + "₾";
+ 
+
+// })
+
+let  x;
+ 
+let string = 'მე მიყვარს პროგრამირება';
+let numb = 52;
+let functio = function (){};
+let array = [1,2];
+let obj = {};
+let undefin = undefined;
+let nul = null;
+let bool = true;
+let boolfal = false;
+let nan = NaN;
+
+// console.log(5 == 5)
+
+document.getElementById("output").innerHTML = bool;
+
+ 
